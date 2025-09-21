584181b15954df27205a083c9a8e7779ba29fe2b
sang-dang-19 <danganhsang09@gmail.com>
2025-09-21 17:35:59 +0700

    Fix update user account

--- src/common/dto/auth/UpdateUserAccount.dto.ts
+++ src/common/dto/auth/UpdateUserAccount.dto.ts
@@ -1,41 +1,38 @@
 import {
-  IsNotEmpty,
   IsOptional,
   IsPhoneNumber,
   IsUrl,
   MaxLength,
+  MinLength,
 } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserAccountDto {
   @IsOptional()
-  @IsNotEmpty()
+  @MinLength(1)
   @MaxLength(255)
   @ApiProperty({ default: 'John' })
   firstName?: string;
 
   @IsOptional()
-  @IsNotEmpty()
+  @MinLength(1)
   @MaxLength(255)
   @ApiProperty({ default: 'Doe' })
   lastName?: string;
 
   @IsOptional()
-  @IsNotEmpty()
   @IsPhoneNumber()
   @MaxLength(255)
   @ApiProperty({ default: '+1234567890' })
   phoneNumber?: string;
 
   @IsOptional()
-  @IsNotEmpty()
   @IsUrl()
   @MaxLength(1000)
   @ApiProperty({ default: 'https://picsum.photos/id/64/800/800' })
   avatarUrl?: string;
 
   @IsOptional()
-  @IsNotEmpty()
   @IsUrl()
   @MaxLength(1000)
   @ApiProperty({ default: 'https://picsum.photos/id/80/1920/1080' })
