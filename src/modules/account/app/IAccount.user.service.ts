94dc9ec5fd1e603e3ccde96085adcbc64e938736
sang-dang-19 <danganhsang09@gmail.com>
2025-09-23 00:18:18 +0700

    Refactors and move onboarding to account module

--- src/modules/account/app/IAccount.user.service.ts
+++ src/modules/account/app/IAccount.user.service.ts
@@ -1,7 +1,10 @@
 import { UserAccountResponse } from '@/common/dto/auth/UserAccountResponse.dto';
 import { UserGetAccountInfo } from '@/common/dto/auth/UserGetAccountInfo.dto';
+import { OnboardUser } from '@/common/dto/auth/Onboarding.dto';
+import { UpdateResult } from 'typeorm';
 
 export const IAccountUserService = Symbol('IAccountUserService');
 export interface IAccountUserService {
   getAccountInfo(dto: UserGetAccountInfo.Dto): Promise<UserAccountResponse.Dto>;
+  onboardUser(accountId: string, dto: OnboardUser.DTO): Promise<UpdateResult>;
 }
---
+++ src/modules/account/infra/repository/UserTags.repository.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { UserTagsEntity } from '@/modules/account/domain/UserTags.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UserTagsRepository {
+  constructor(
+    @InjectRepository(UserTagsEntity)
+    private readonly repo: Repository<UserTagsEntity>,
+  ) {}
+}
