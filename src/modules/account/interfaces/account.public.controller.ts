873b52983f4d688f616636edf89a923f59df92e8
sang-dang-19 <danganhsang09@gmail.com>
2025-09-22 15:56:13 +0700

    Make Get users by ID public

---
+++ src/modules/account/interfaces/account.public.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
+import { AccountUserService } from '@/modules/account/app/account.user.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Account - User')
+@Controller('/public/account')
+export class AccountUserController {
+  constructor(private readonly accountUserService: AccountUserService) {}
+
+  @ApiOperation({ summary: 'Get user details by id' })
+  @Get('/info/:id')
+  getAccountInfo(@Param('id', ParseUUIDPipe) id: string) {
+    return this.accountUserService.getAccountInfo({ userId: id });
+  }
+}
