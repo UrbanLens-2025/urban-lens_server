884221f133c29306235451636d660e419b30d54f
Tunn2 <ktung7574@gmail.com>
2025-10-16 11:36:34 +0700

    Add: Implement createLocation method in LocationBusinessController
    - Introduce CreateLocationDto for handling location creation requests.
    - Add a new POST endpoint for business owners to create a new location.

--- src/modules/business/interfaces/Location.business.controller.ts
+++ src/modules/business/interfaces/Location.business.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Inject, Param, Patch } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ILocationService } from '../app/ILocation.service';
 import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Role } from '@/common/constants/Role.constant';
@@ -6,6 +14,7 @@ import { Roles } from '@/common/Roles.decorator';
 import { JwtTokenDto } from '@/common/dto/JwtToken.dto';
 import { AuthUser } from '@/common/AuthUser.decorator';
 import { UpdateLocationDto } from '@/common/dto/location/UpdateLocation.dto';
+import { CreateLocationDto } from '@/common/dto/location/CreateLocation.dto';
 
 @ApiBearerAuth()
 @ApiTags('Location - Business')
@@ -15,6 +24,16 @@ export class LocationBusinessController {
     @Inject(ILocationService)
     private readonly locationService: ILocationService,
   ) {}
+
+  @Post()
+  @Roles(Role.BUSINESS_OWNER)
+  @ApiOperation({ summary: 'Create a new location' })
+  async createLocation(
+    @Body() createLocationDto: CreateLocationDto,
+    @AuthUser() user: JwtTokenDto,
+  ) {
+    return this.locationService.createLocation(createLocationDto, user.sub);
+  }
 
   @Get('my-locations')
   @Roles(Role.BUSINESS_OWNER)
