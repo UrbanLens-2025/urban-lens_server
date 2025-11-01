27dfd9c3e76691522d536f537d7849e812eadb2a
sang-dang-19 <danganhsang09@gmail.com>
2025-11-01 09:53:47 +0700

    Disable sync

--- src/config/persistence.config.ts
+++ src/config/persistence.config.ts
@@ -1,15 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
 import { Environment } from '@/config/env.config';
 
 @Injectable({})
 export class PersistenceConfig implements TypeOrmOptionsFactory {
+  private readonly logger = new Logger(PersistenceConfig.name);
+
   constructor(private readonly configService: ConfigService<Environment>) {}
 
   createTypeOrmOptions(
     connectionName?: string,
   ): Promise<TypeOrmModuleOptions> | TypeOrmModuleOptions {
+    if (this.configService.get('DATABASE_ENABLE_SYNC')) {
+      this.logger.warn(
+        'Database synchronization is not supported. Please write migration scripts.',
+      );
+    }
+
     return {
       type: 'postgres',
       host: this.configService.get('DATABASE_HOST'),
@@ -17,7 +25,7 @@ export class PersistenceConfig implements TypeOrmOptionsFactory {
       username: this.configService.get('DATABASE_USER'),
       password: this.configService.get('DATABASE_PASSWORD'),
       database: this.configService.get('DATABASE_NAME'),
-      synchronize: this.configService.get('DATABASE_ENABLE_SYNC'),
+      synchronize: false,
       schema: this.configService.get('DATABASE_SCHEMA'),
       logging: 'all',
       autoLoadEntities: true,
