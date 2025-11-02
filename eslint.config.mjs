bb3bd2fde4ad416f28fe1145ba91130d9ea8ffd6
sang-dang-19 <danganhsang09@gmail.com>
2025-11-02 17:45:32 +0700

    Add Migrations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['eslint.config.mjs', './db/migrations/**/*.ts'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
@@ -31,7 +31,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-argument': 'warn',
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-namespace': 'off',
-      'prettier/prettier': 'warn'
+      'prettier/prettier': 'warn',
     },
   },
 );
