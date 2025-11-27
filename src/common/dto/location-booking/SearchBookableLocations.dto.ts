cb82490e87a177350ec4af460bf625b206927e82
sang-dang-19 <danganhsang09@gmail.com>
2025-11-28 05:45:31 +0700

    feat(location): Add filters and search to query for bookable locations

--- src/common/dto/location-booking/SearchBookableLocations.dto.ts
+++ src/common/dto/location-booking/SearchBookableLocations.dto.ts
@@ -2,4 +2,10 @@ import { type PaginateQuery } from 'nestjs-paginate';
 
 export class SearchBookableLocationsDto {
   query: PaginateQuery;
+
+  // filtering by available dates is harder so imma do this manually
+  bookingDates?: {
+    startDate: Date;
+    endDate: Date;
+  };
 }
--- src/modules/location-booking/interfaces/Location.creator.controller.ts
+++ src/modules/location-booking/interfaces/Location.creator.controller.ts
@@ -1,7 +1,20 @@
-import { Controller, Get, Inject, Param, ParseUUIDPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Inject,
+  Param,
+  ParseDatePipe,
+  ParseUUIDPipe,
+  Query,
+} from '@nestjs/common';
 import { Roles } from '@/common/Roles.decorator';
 import { Role } from '@/common/constants/Role.constant';
-import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 import {
   ApiPaginationQuery,
   Paginate,
@@ -11,6 +24,7 @@ import {
   IBookableLocationSearchService,
   IBookableLocationSearchService_QueryConfig,
 } from '@/modules/location-booking/app/IBookableLocationSearch.service';
+import dayjs from 'dayjs';
 
 @ApiTags('Location')
 @ApiBearerAuth()
@@ -26,10 +40,29 @@ export class LocationCreatorController {
   @ApiPaginationQuery(
     IBookableLocationSearchService_QueryConfig.searchBookableLocations(),
   )
+  @ApiQuery({
+    name: 'startDate',
+    type: Date,
+    required: false,
+    example: dayjs().add(1, 'day').toDate(),
+  })
+  @ApiQuery({
+    name: 'endDate',
+    type: Date,
+    required: false,
+    example: dayjs().add(2, 'days').toDate(),
+  })
   @Get('/search')
-  searchBookableLocations(@Paginate() query: PaginateQuery) {
+  searchBookableLocations(
+    @Paginate() query: PaginateQuery,
+    @Query('startDate', new ParseDatePipe({ optional: true }))
+    startDate?: Date | null,
+    @Query('endDate', new ParseDatePipe({ optional: true }))
+    endDate?: Date | null,
+  ) {
     return this.bookableLocationSearchService.searchBookableLocations({
       query,
+      bookingDates: startDate && endDate ? { startDate, endDate } : undefined,
     });
   }
   @ApiOperation({ summary: 'Get Bookable Location by ID' })
