9ba571f2b1291296645a6add3bcc9021f4d040f1
sang-dang-19 <danganhsang09@gmail.com>
2025-12-03 16:53:51 +0700

    Remove Location Opening hours

--- src/common/dto/business/GetLocationOpeningHours.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsUUID } from 'class-validator';
-
-export class GetLocationOpeningHoursDto {
-  @IsNotEmpty()
-  @IsUUID()
-  @ApiProperty()
-  locationId: string;
-}
